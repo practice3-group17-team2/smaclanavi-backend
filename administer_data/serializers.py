cd396c222b4e01ce3ef425aa6b4db0b0ef2a7169
iniad <s1f101901342@iniad.org>
2022-08-30 20:37:10 +0900

    Modified to use ModelSerializer

--- administer_data/serializers.py
+++ administer_data/serializers.py
@@ -2,38 +2,13 @@ from rest_framework import serializers
 from administer_data.models import ClassInfo
 
 
-class ClassInfoSerializer(serializers.Serializer):
+class ClassInfoSerializer(serializers.ModelSerializer):
     """
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
     """
-    id = serializers.IntegerField(read_only=True)
-    class_name = serializers.CharField()
-    phone_number = serializers.CharField(required=False, allow_blank=True,max_length=100)
-    address = serializers.CharField(required=False, allow_blank=True,max_length=100)
-    evaluation = serializers.IntegerField()
-    price = serializers.IntegerField()
-    site_url = serializers.URLField()
-
-    def create(self, validated_data):
-        """
-        Create and return a new `ClassInfo` instance, given the validated data.
-        """
-        return ClassInfo.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `ClassInfo` instance, given the validated data.
-        """
-        instance.class_name = validated_data.get('class_name', instance.class_name)
-        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-        instance.address = validated_data.get('address', instance.address)
-        instance.evaluation = validated_data.get('evaluation', instance.evaluation)
-        instance.price = validated_data.get('price', instance.price)
-        instance.site_url = validated_data.get('site_url', instance.site_url)
-        instance.save()
-        return instance
+    class Meta:
+        model = ClassInfo
+        fields = ['id', 'class_name', 'phone_number', 'address', 'evaluation', 'price', 'site_url']
+        # city, lecture
