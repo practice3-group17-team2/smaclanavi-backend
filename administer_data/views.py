ce112253c4f306cd407b43dca8e7593aa7985e9d
iniad <s1f101901342@iniad.org>
2022-08-30 21:38:37 +0900

    Modified to use generic class-based views(tutorial:3)

--- administer_data/views.py
+++ administer_data/views.py
@@ -1,32 +1,13 @@
 from administer_data.models import ClassInfo
 from administer_data.serializers import ClassInfoSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class ClassInfoList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+class ClassInfoList(generics.ListCreateAPIView):
     queryset = ClassInfo.objects.all()
     serializer_class = ClassInfoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class ClassInfoDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class ClassInfoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ClassInfo.objects.all()
     serializer_class = ClassInfoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
