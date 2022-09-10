eed59c0174ae2ea6e973288306cd361e127510dc
iniad <s1f101901342@iniad.org>
2022-09-10 19:53:40 +0900

    Added root end point, added name to urls

--- administer_data/urls.py
+++ administer_data/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from administer_data import views
 
-urlpatterns = [
-    path('classinfo/', views.ClassInfoList.as_view()),
-    path('classinfo/<int:pk>/', views.ClassInfoDetail.as_view()),
-    path('review/', views.ReviewList.as_view()),
-    path('review/<int:pk>/', views.ReviewDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('classinfo/', 
+        views.ClassInfoList.as_view(), 
+        name='classinfo-list'),
+    path('classinfo/<int:pk>/', 
+        views.ClassInfoDetail.as_view(), 
+        name='classinfo-detail'),
+    path('review/', 
+        views.ReviewList.as_view(), 
+        name='review-list'),
+    path('review/<int:pk>/', 
+        views.ReviewDetail.as_view(), 
+        name='review-detail'),
+])
--- administer_data/views.py
+++ administer_data/views.py
@@ -1,7 +1,16 @@
 from administer_data.models import ClassInfo, Review
 from administer_data.serializers import ClassInfoSerializer, ReviewSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'classinfo': reverse('classinfo-list', request=request, format=format),
+        'reviews': reverse('review-list', request=request, format=format)
+    })
 
 class ClassInfoList(generics.ListCreateAPIView):
     queryset = ClassInfo.objects.all()
