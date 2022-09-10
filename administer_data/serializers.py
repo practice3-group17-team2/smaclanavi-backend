cab5af333e6a1a3ea633c1aa6fb1d720778fa70a
iniad <s1f101901342@iniad.org>
2022-09-10 20:18:59 +0900

    mdf hyperlinked view_name=review-detail

--- administer_data/serializers.py
+++ administer_data/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from administer_data.models import ClassInfo, Lecture, Review
-# from administer_data.models import Prefecture, City
+from administer_data.models import Prefecture, City
 
-""" 
+
 class PrefectureSerializer(serializers.ModelSerializer):
     class Meta:
             model = Prefecture
@@ -12,21 +12,25 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
             model = City
             fields = ['id','city_name', 'prefecture']
-"""
+
 
 class LectureSerializer(serializers.ModelSerializer):
     class Meta:
             model = Lecture
             fields = ['id', 'lecture_content', 'is_target_old']
 
-class ClassInfoSerializer(serializers.ModelSerializer):
+# class ClassInfoSerializer(serializers.ModelSerializer):
+class ClassInfoSerializer(serializers.HyperlinkedModelSerializer):
     """
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
     """
-  # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-    reviews = serializers.PrimaryKeyRelatedField(many=True, queryset=Review.objects.all())
+    # reviews = serializers.PrimaryKeyRelatedField(many=True, queryset=Review.objects.all())
+    reviews = serializers.HyperlinkedIdentityField(view_name='review-detail')
+    
+    city = CitySerializer(required=False)
+    
     class Meta:
         model = ClassInfo
         fields = ['id', 'class_name', 'phone_number', 'address', 'evaluation', 'price', 'site_url', 'city', 'lecture', 'reviews']
