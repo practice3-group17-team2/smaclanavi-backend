5daa18938b87e2d79bb3d345f86669ca0f7e1367
iniad <s1f101901342@iniad.org>
2022-09-12 14:52:48 +0900

    mdf relational field default pk

--- administer_data/models.py
+++ administer_data/models.py
@@ -9,21 +9,22 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 """
 
 class Prefecture(models.Model):
-    pref_name = models.CharField(max_length=20)
+    pref_name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.pref_name
 
 class City(models.Model):
-    city_name  = models.CharField(max_length=20)
-    prefecture = models.ForeignKey(Prefecture, null=True, on_delete=models.SET_NULL)
+    city_name  = models.CharField(max_length=20, unique=True)
+    # pref instance(pk=1) = 未設定用
+    prefecture = models.ForeignKey(Prefecture, blank=True, on_delete=models.SET_DEFAULT, default=1)
 
     def __str__(self):
         return self.city_name
 
 class Lecture(models.Model):
     created         = models.DateTimeField(auto_now_add=True)
-    lecture_content = models.CharField(max_length=20)
+    lecture_content = models.CharField(max_length=20, unique=True)
     is_target_old   = models.BooleanField(blank=True, default=True)
 
     def __str__(self):
@@ -33,12 +34,13 @@ class ClassInfo(models.Model):
     created      = models.DateTimeField(auto_now_add=True)
     class_name   = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100, blank=True, default='')
+    # city instance(pk=1) = 未設定用
+    city         = models.ForeignKey(City, blank=True, on_delete=models.SET_DEFAULT, default=1)
     address      = models.CharField(max_length=100, blank=True, default='')
+    lecture      = models.ManyToManyField(Lecture, related_name="class_info")
     evaluation   = models.IntegerField(blank=True, default=0)
     price        = models.IntegerField(blank=True, default=0)
     site_url     = models.URLField(blank=True, default='')
-    city         = models.ForeignKey(City, on_delete=models.SET_DEFAULT, blank=True, null=True, default=None)
-    lecture      = models.ManyToManyField(Lecture, related_name="class_info")
 
     class Meta:
         ordering = ['created']
@@ -51,7 +53,7 @@ class Review(models.Model):
     class_info  = models.ForeignKey(ClassInfo, on_delete=models.CASCADE, related_name='reviews')
     review_text = models.TextField(max_length=200)
     faves       = models.IntegerField(blank=True, default=0)
-    author      = models.CharField(max_length=20 ,blank=True, default="名無し")
+    author      = models.CharField(max_length=20 ,blank=True, default='名無し')
 
     def __str__(self) -> str:
         return super().__str__() + ":" + self.review_text[:5]
