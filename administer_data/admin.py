a0e9fbb1a26fc70203f649ea398c371211118072
iniad <s1f101901342@iniad.org>
2022-09-15 18:51:20 +0900

    mdf admin page

--- administer_data/admin.py
+++ administer_data/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Prefecture, City, Lecture, ClassInfo, Review
 # Register your models here.
+
+class PrefectureAdmin(admin.ModelAdmin):
+    fields = ['pref_name']
+class CityAdmin(admin.ModelAdmin):
+    fields = ['city_name', 'prefecture']
+class LectureAdmin(admin.ModelAdmin):
+    fields = ['lecture_content', 'is_target_old']
+class ClassInfoAdmin(admin.ModelAdmin):
+    fields = ['class_name', 'phone_number', 'city', 'address', 'lecture', 'price', 'site_url']
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ['class_info', 'review_text', 'faves', 'author']
+
+admin.site.register(Prefecture, PrefectureAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Lecture, LectureAdmin)
+admin.site.register(ClassInfo, ClassInfoAdmin)
+admin.site.register(Review, ReviewAdmin)
