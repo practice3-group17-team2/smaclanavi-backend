678ffe874d43ae7937f53f011cf1115f29334a0e
iniad <cornforprogramming@gmail.com>
2022-11-01 22:49:52 +0900

    [add] seleniumを用いたスクレイピングの実装(一部)、検索結果の店のURLを取得

--- administer_data/scraping.py
+++ administer_data/scraping.py
@@ -1,4 +1,7 @@
 import sys, bs4, requests
+from typing_extensions import override
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 SEARCH_NUM = "100"
 
@@ -22,52 +25,80 @@ class ScrapingBase:
         except Exception as e:
             print("ERROR_FORMAT:{}".format(e))
         return url
-    
+
+    @classmethod
+    def crawling_data(cls, url: str):
+        #result = requests.get(url, timeout=TIMEOUT, headers=HEADER)
+        result = requests.get(url, timeout=cls.TIMEOUT, headers=cls.HEADER)
+        result.raise_for_status()
+        return result
+
     @classmethod
     def scrape_data(cls, url: str, selecter: str) -> list:
         """urlからselecterに該当する要素を抽出して返す関数"""
-        ret = []
+        ret = None
         try:
-            #result = requests.get(url, timeout=TIMEOUT, headers=HEADER)
-            result = requests.get(url, timeout=cls.TIMEOUT, headers=cls.HEADER)
-
-            result.raise_for_status()
+            result = cls.crawling_data(url)
         except Exception as e:
             print("ERROR_DOWNLOAD:{}".format(e))
         else:
             soup = bs4.BeautifulSoup(result.content, 'html.parser')
             ret = soup.select(selecter)
-            ls = soup.select("#js-shop-list")
-        return ret, ls
+        return ret
 
-class SoftbankShopScraping(ScrapingBase):
+
+class ScrapingSeleBase(ScrapingBase):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(chrome_options=options)
+
+    @classmethod
+    def crawling_data(cls, url: str):
+        cls.driver.get(url)
+        result = cls.driver.page_source.encode('utf-8')
+        return result
+
+    @classmethod
+    def scrape_data(cls, url: str, selecter: str) -> list:
+        """urlからselecterに該当する要素を抽出して返す関数"""
+        ret = None
+        try:
+            result = cls.crawling_data(url)
+        except Exception as e:
+            print("ERROR_DOWNLOAD:{}".format(e))
+        else:
+            soup = bs4.BeautifulSoup(result, 'html.parser')
+            ret = soup.select(selecter)
+        return ret
+
+
+class SoftbankShopScraping(ScrapingSeleBase):
     # ソフトバンク 東京都北区
     # https://www.softbank.jp/shop/search/list/?spadv=on&pref=13&area=131172&cid=tpsk_191119_mobile
     area_url_format = "https://www.softbank.jp/shop/search/list/?spadv=on&pref={0[pref]}&area={0[area]}&cid=tpsk_191119_mobile"
 
     tmp_args = {
-        "tokyo-kita":{
+        "tokyo-kita": {
             "pref": 13,
             "area": 131172,
         },
     }
 
-    area_url = ScrapingBase.url_from_format(area_url_format, **tmp_args["tokyo-kita"])
+    area_url = ScrapingBase.url_from_format(area_url_format,
+                                            **tmp_args["tokyo-kita"])
     shop_link_selecter = "#js-shop-list > ul > li > div.shop-page-u96-shop-list-item_headder > h3 > a"
-    
-    
+
     @classmethod
     def check_st_var(cls):
         print(cls.area_url)
         print(cls.shop_link_selecter)
 
     @classmethod
-    def scraping_shop(cls):
+    def scraping_shop(cls) -> list:
+        """ 店舗のurlのリストを返す """
         shop_links_a = cls.scrape_data(cls.area_url, cls.shop_link_selecter)
-        print(1,shop_links_a)
         shop_links = [link_tag.get("href") for link_tag in shop_links_a]
         return shop_links
-
 
 
 #グーグル検索から検索結果のリストを返す関数
--- administer_data/tests.py
+++ administer_data/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from .savedata import save_data
 from .scraping import SoftbankShopScraping as sss
+from .scraping import ScrapingSeleBase as ssb
 
 # Create your tests here.
 
@@ -15,15 +16,30 @@ class URLTest(TestCase):
     #     sss.check_st_var()
 
     def test_scraping_shop(self):
+        # 想定した答え
         ls = [
-            "/shop/search/detail/TD43/?cid=tpsk_191119_mobile",
-            "/shop/search/detail/TD20/?cid=tpsk_191119_mobile"
-            ]
-        #self.assertIs(sss.scraping_shop(), ls)
-    
+            '/shop/search/detail/TD43/?cid=tpsk_191119_mobile',
+            '/shop/search/detail/TD20/?cid=tpsk_191119_mobile'
+        ]
+        result = sss.scraping_shop()
+        print(result)
+        # 結果
+        # ['/shop/search/detail/TD43/?cid=tpsk_191119_mobile',
+        # '/shop/search/detail/TD20/?cid=tpsk_191119_mobile']
+        self.assertEqual(result, ls)
+
+    """ 
     def test_scrape_data(self):
-        print("aiueo",sss.scrape_data(sss.area_url, sss.shop_link_selecter))
-    
+        print("aiueo", sss.scrape_data(sss.area_url, sss.shop_link_selecter))
+
+    def test_scraping_sele_crawling(self):
+        ssb.crawling_data(sss.area_url)
+
+    def test_scraping_sele(self):
+        print(ssb.scrape_data(sss.area_url, sss.shop_link_selecter))
+    """
+
+
 # url = "https://www.softbank.jp/shop/search/list/?spadv=on&pref=13&area=131172&cid=tpsk_191119_mobile"
 # selecter = "#js-shop-list > ul > li > div.shop-page-u96-shop-list-item_headder > h3 > a"
 # selecter = "#js-shop-list > ul.shop-page-u96-shop-list"
