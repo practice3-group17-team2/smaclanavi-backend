00ea16259c6fe41429dc384856294c3b22525562
iniad <cornforprogramming@gmail.com>
2022-11-01 21:29:07 +0900

    [update] beautiful soupによるスクレイピング実装、jsによるコンテンツ生成なので要selenium

--- administer_data/scraping.py
+++ administer_data/scraping.py
@@ -1,17 +1,78 @@
 import sys, bs4, requests
 
 SEARCH_NUM = "100"
-TIMEOUT = 10
-HEADER = {
-    'User-Agent':
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
-}
 
-# HEADER      = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
+
+class ScrapingBase:
+    TIMEOUT = 10
+    HEADER = {
+        'User-Agent':
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
+    }
+
+    # HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+    #                           AppleWebKit/537.36 (KHTML, like Gecko)
+    #                           Chrome/105.0.0.0 Safari/537.36'}
+    @classmethod
+    def url_from_format(cls, url_format: str, **kwargs) -> str:
+        """url_format.format(**kwargs)をするだけの関数"""
+        url = ""
+        try:
+            url = url_format.format(kwargs)
+        except Exception as e:
+            print("ERROR_FORMAT:{}".format(e))
+        return url
+    
+    @classmethod
+    def scrape_data(cls, url: str, selecter: str) -> list:
+        """urlからselecterに該当する要素を抽出して返す関数"""
+        ret = []
+        try:
+            #result = requests.get(url, timeout=TIMEOUT, headers=HEADER)
+            result = requests.get(url, timeout=cls.TIMEOUT, headers=cls.HEADER)
+
+            result.raise_for_status()
+        except Exception as e:
+            print("ERROR_DOWNLOAD:{}".format(e))
+        else:
+            soup = bs4.BeautifulSoup(result.content, 'html.parser')
+            ret = soup.select(selecter)
+            ls = soup.select("#js-shop-list")
+        return ret, ls
+
+class SoftbankShopScraping(ScrapingBase):
+    # ソフトバンク 東京都北区
+    # https://www.softbank.jp/shop/search/list/?spadv=on&pref=13&area=131172&cid=tpsk_191119_mobile
+    area_url_format = "https://www.softbank.jp/shop/search/list/?spadv=on&pref={0[pref]}&area={0[area]}&cid=tpsk_191119_mobile"
+
+    tmp_args = {
+        "tokyo-kita":{
+            "pref": 13,
+            "area": 131172,
+        },
+    }
+
+    area_url = ScrapingBase.url_from_format(area_url_format, **tmp_args["tokyo-kita"])
+    shop_link_selecter = "#js-shop-list > ul > li > div.shop-page-u96-shop-list-item_headder > h3 > a"
+    
+    
+    @classmethod
+    def check_st_var(cls):
+        print(cls.area_url)
+        print(cls.shop_link_selecter)
+
+    @classmethod
+    def scraping_shop(cls):
+        shop_links_a = cls.scrape_data(cls.area_url, cls.shop_link_selecter)
+        print(1,shop_links_a)
+        shop_links = [link_tag.get("href") for link_tag in shop_links_a]
+        return shop_links
+
 
 
 #グーグル検索から検索結果のリストを返す関数
 def search_google(words):
+    sb = ScrapingBase
     """
     検索する言葉を受け取ってlink_list, title_listを返す
     """
@@ -20,7 +81,7 @@ def search_google(words):
 
     try:
         url = "https://www.google.co.jp/search?q=" + words + "&num=" + SEARCH_NUM + "&start=0"
-        result = requests.get(url, timeout=TIMEOUT, headers=HEADER)
+        result = requests.get(url, timeout=sb.TIMEOUT, headers=sb.HEADER)
 
         result.raise_for_status()
     except Exception as e:
@@ -39,7 +100,7 @@ def search_google(words):
 
         #デバッグ用のprint
         #print("length {}, {}".format(len(link_list), len(title_list)))
-        
+
         #タイトルとリンクが不一致の場合、
         if len(link_list) != len(title_list):
             return [], []
@@ -47,7 +108,7 @@ def search_google(words):
     return link_list, title_list
 
 
-#""" 
+#"""
 def main():
 
     words = input("検索ワードを入力してください")
--- administer_data/tests.py
+++ administer_data/tests.py
@@ -1,10 +1,29 @@
 from django.test import TestCase
 
 from .savedata import save_data
+from .scraping import SoftbankShopScraping as sss
 
 # Create your tests here.
-class ScrapingTest(TestCase):
-    def test_save_data(self):
-        save_data()
+
+# class ScrapingTest(TestCase):
+#     def test_save_data(self):
+#         save_data()
 
 
+class URLTest(TestCase):
+    # def test_check_st_var(self):
+    #     sss.check_st_var()
+
+    def test_scraping_shop(self):
+        ls = [
+            "/shop/search/detail/TD43/?cid=tpsk_191119_mobile",
+            "/shop/search/detail/TD20/?cid=tpsk_191119_mobile"
+            ]
+        #self.assertIs(sss.scraping_shop(), ls)
+    
+    def test_scrape_data(self):
+        print("aiueo",sss.scrape_data(sss.area_url, sss.shop_link_selecter))
+    
+# url = "https://www.softbank.jp/shop/search/list/?spadv=on&pref=13&area=131172&cid=tpsk_191119_mobile"
+# selecter = "#js-shop-list > ul > li > div.shop-page-u96-shop-list-item_headder > h3 > a"
+# selecter = "#js-shop-list > ul.shop-page-u96-shop-list"
