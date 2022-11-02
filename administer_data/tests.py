1ff70ae0afd31c3312116415417e956a9377699c
iniad <cornforprogramming@gmail.com>
2022-11-02 20:26:00 +0900

    [add] エリアから店舗を取得して、そこから各店舗の情報取得するTestを追加

--- administer_data/tests.py
+++ administer_data/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 
 from .savedata import save_data
-from .scraping import SoftbankShopScraping as sss
+from .scraping import SBgetShopURLs as sss
 from .scraping import ScrapingSeleBase as ssb
 from .scraping import IsNeedSele
 from .scraping import SBgetShopInfo
+from .scraping import SBgetShopURLs
 
 # Create your tests here.
 
@@ -14,10 +15,31 @@ from .scraping import SBgetShopInfo
 
 class TestDefault(TestCase):
     def test_scrape_shop_info(self):
+        """ 
+        店舗単体の情報取得
+        """
         # 赤羽店
         url = "https://www.softbank.jp/shop/search/detail/TD20/"
         data = SBgetShopInfo.scraping_info(url)
         print(data)
+
+    def test_scrape_shops(self):
+        """ 
+        エリアから店舗を取得して、そこから各店舗の情報取得
+        """
+        datas = {}
+        base_url = "https://www.softbank.jp"
+        url = "https://www.softbank.jp/shop/search/list/?spadv=on&pref=13&area=131172&cid=tpsk_191119_mobile"
+        shop_url_xs= SBgetShopURLs.scraping_shop_urls(url)
+        print(shop_url_xs,"\n"*3)
+
+        for shop_url in shop_url_xs:
+            shop_url = base_url+shop_url
+            data = SBgetShopInfo.scraping_info(shop_url)
+            datas[data["name"]] = data
+        print(datas)
+
+
 
 
 class TestIsNeedSele(TestCase):
@@ -39,12 +61,13 @@ class URLTest(TestCase):
     #     sss.debug_check_st_var()
 
     def test_scraping_shop(self):
+        url = "https://www.softbank.jp/shop/search/list/?spadv=on&pref=13&area=131172&cid=tpsk_191119_mobile"
         # 想定した答え
         ls = [
             '/shop/search/detail/TD43/?cid=tpsk_191119_mobile',
             '/shop/search/detail/TD20/?cid=tpsk_191119_mobile'
         ]
-        result = sss.scraping_shop()
+        result = sss.scraping_shop_urls(url)
         print(result)
         # 結果
         # ['/shop/search/detail/TD43/?cid=tpsk_191119_mobile',
