46f18144760e77a9cd021e439555343de2dde4f8
iniad <cornforprogramming@gmail.com>
2022-11-23 12:49:23 +0900

    [update] serializerに存在しないデータを指定してPOST or PUTされたときにエラーで終了しないように修正

--- administer_data/views.py
+++ administer_data/views.py
@@ -3,6 +3,7 @@ from administer_data import models, serializers
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.exceptions import APIException
+from rest_framework.exceptions import NotFound
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -20,48 +21,45 @@ def api_root(request, format=None):
     })
 
 
-class ClassInfoViewSet(viewsets.ModelViewSet):
-    queryset = models.ClassInfo.objects.all()
-    serializer_class = serializers.ClassInfoSerializer
-
-
-class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = models.Review.objects.all()
-    serializer_class = serializers.ReviewSerializer
-
-
 class BadRequest(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "bad request"
     default_code = "bad_request"
 
 
-class UpcomingLecInfoViewSet(viewsets.ModelViewSet):
-    queryset = models.UpcomingLecInfos.objects.all()
-    serializer_class = serializers.UpcomingLecInfoSerializer
+class CatchExceptionViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
-        """
-        serializerのcreateから渡されるcreatedを取得して
-        新規作成か取得しただけかでhttp responseを場合分けしたかったが、取得出来ず諦めた
-        以下はCreateModelMixinをそのままコピペしただけ
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
+        try:
+            return super().create(request, *args, **kwargs)
+        except Exception as e:
+            raise BadRequest(detail=e.args)
+
     def update(self, request, *args, **kwargs):
-        """
-        serializerが送出したvalue errorによってステータスコードを変更
-        """
         try:
             return super().update(request, *args, **kwargs)
-        except ValueError as e:
+
+        # except models.Lecture.DoesNotExist as e:
+        #     raise NotFound(detail=e.args)
+        except Exception as e:
             raise BadRequest(detail=e.args)
 
 
-class LecScheduleViewSet(viewsets.ModelViewSet):
+class ClassInfoViewSet(CatchExceptionViewSet):
+    queryset = models.ClassInfo.objects.all()
+    serializer_class = serializers.ClassInfoSerializer
+
+
+class ReviewViewSet(CatchExceptionViewSet):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+
+class UpcomingLecInfoViewSet(CatchExceptionViewSet):
+    queryset = models.UpcomingLecInfos.objects.all()
+    serializer_class = serializers.UpcomingLecInfoSerializer
+
+
+class LecScheduleViewSet(CatchExceptionViewSet):
     queryset = models.LecSchedule.objects.all()
     serializer_class = serializers.LecScheduleSerializer
