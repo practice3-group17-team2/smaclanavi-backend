10f758037b057c54d932b9147eb740878cacb6f4
iniad <cornforprogramming@gmail.com>
2022-11-28 00:44:49 +0900

    [update] データの保存の基本機能をクラスで継承できるように修正

---
+++ administer_data/scraping/save_data.py
@@ -0,0 +1,51 @@
+import re
+import os
+import pickle
+
+from administer_data import models
+from administer_data.scraping.softbank.sb_scrape_class_info import SBscraping as sb
+# from administer_data.scraping.data import softbank_data as sb_data
+
+
+class BaseDataRecoder():
+
+    @classmethod
+    def save_data_to_pkl_file(cls, data: dict, file_path):
+        with open(
+                os.path.join("./administer_data/scraping/data/",
+                             file_path + ".pkl"), 'wb') as f:
+            pickle.dump(data, f)
+
+    @classmethod
+    def load_data_from_pkl_file(cls, file_path) -> dict:
+        with open(
+                os.path.join('./administer_data/scraping/data/',
+                             file_path + ".pkl"), 'rb') as f:
+            dict_pkl = pickle.load(f)
+        return dict_pkl
+
+    @classmethod
+    def save_fixed_data_to_model(cls, src_file="softbank_test"):
+        """ 
+        pklファイルに保存されたデータからインスタンスを作成しモデルに保存する関数
+        scrapingする関数の呼び出しや情報のUPDATEとかもできたらいいなと思っている
+        """
+        data = cls.load_data_from_pkl_file(file_path=src_file)
+        # 任意の処理
+
+    # def dict_to_pkl_file():
+    #     # print(sb_data.data)
+
+    #     sb.save_data_file_pkl(sb_data.data, "softbank-2022_11_26_14_50_49")
+    #     # sb.save_data_file_pkl(sb_data.data, "softbank_test")
+
+    @classmethod
+    def fix_data(cls, src_file="softbank_test"):
+        data = cls.load_data_from_pkl_file(src_file)
+        # print(data)
+        for key, info_xs in data.items():
+            # 任意の処理
+            pass
+
+        # print(data)
+        cls.save_data_to_pkl_file(data, file_path=src_file + "_fixed")
--- administer_data/scraping/softbank/sb_savedata.py
+++ administer_data/scraping/softbank/sb_savedata.py
@@ -2,87 +2,79 @@ import re
 import os
 import pickle
 
-from administer_data.scraping.softbank.sb_scrape_class_info import SBscraping as sb
 from administer_data import models
+from administer_data.scraping.save_data import BaseDataRecoder
 # from administer_data.scraping.data import softbank_data as sb_data
 
+class SoftBankDataRecorder(BaseDataRecoder):
+    @classmethod
+    def save_data_to_pkl_file(cls, data: dict, file_path):
+        return super().save_data_to_pkl_file(data, "softbank/"+file_path)
 
-def save_data_to_pkl_file(data: dict, file_path):
-    with open(
-            os.path.join("./administer_data/scraping/data/softbank/",
-                         file_path + ".pkl"), 'wb') as f:
-        pickle.dump(data, f)
+    @classmethod
+    def load_data_from_pkl_file(cls, file_path) -> dict:
+        return super().load_data_from_pkl_file("softbank/"+file_path)
 
+    @classmethod
+    def save_fixed_data_to_model(cls, src_file="softbank_test"):
+        """ 
+        pklファイルに保存されたデータからインスタンスを作成しモデルに保存する関数
+        scrapingする関数の呼び出しや情報のUPDATEとかもできたらいいなと思っている
+        """
+        data = cls.load_data_from_pkl_file(file_path=src_file)
+        # area = (pref_id, area_id), area_name(店舗数))
+        # class_info_xs = {
+        # "店舗名": {
+        #     'phone_number':
+        #     'class_name':
+        #     'has_parking':
+        #     'is_barrier_free':
+        #     'address':
+        # }
 
-def load_data_from_pkl_file(file_path) -> dict:
-    with open(
-            os.path.join('./administer_data/scraping/data/softbank/',
-                         file_path + ".pkl"), 'rb') as f:
-        dict_pkl = pickle.load(f)
-    return dict_pkl
+        city_name_pattern = re.compile(r'[^0-9（）]+')
 
+        org, _ = models.ClassOrganizer.objects.get_or_create(
+            organizer_name="ソフトバンク")
+        pref = models.Prefecture.objects.get(pref_name="東京都")
 
-def save_fixed_data_to_model(src_file="softbank_test"):
-    """ 
-    pklファイルに保存されたデータからインスタンスを作成しモデルに保存する関数
-    scrapingする関数の呼び出しや情報のUPDATEとかもできたらいいなと思っている
-    """
-    data = load_data_from_pkl_file(file_path=src_file)
-    # area = (pref_id, area_id), area_name(店舗数))
-    # class_info_xs = {
-    # "店舗名": {
-    #     'phone_number':
-    #     'class_name':
-    #     'has_parking':
-    #     'is_barrier_free':
-    #     'address':
-    # }
+        for area, class_info_xs in data.items():
+            if not class_info_xs:
+                continue
 
-    city_name_pattern = re.compile(r'[^0-9（）]+')
+            city_name = city_name_pattern.match(area[1]).group()
+            city, _ = models.City.objects.get_or_create(city_name=city_name,
+                                                        prefecture=pref)
 
-    org, _ = models.ClassOrganizer.objects.get_or_create(
-        organizer_name="ソフトバンク")
-    pref = models.Prefecture.objects.get(pref_name="東京都")
+            for class_info in class_info_xs.values():
+                class_info = models.ClassInfo.objects.create(city=city,
+                                                            class_organizer=org,
+                                                            **class_info)
 
-    for area, class_info_xs in data.items():
-        if not class_info_xs:
-            continue
+        # print(models.City.objects.all())
 
-        city_name = city_name_pattern.match(area[1]).group()
-        city, _ = models.City.objects.get_or_create(city_name=city_name,
-                                                    prefecture=pref)
+    # def dict_to_pkl_file():
+    #     # print(sb_data.data)
 
-        for class_info in class_info_xs.values():
-            class_info = models.ClassInfo.objects.create(city=city,
-                                                         class_organizer=org,
-                                                         **class_info)
-
-    # print(models.City.objects.all())
-    sb.quit_driver()
-
-
-# def dict_to_pkl_file():
-#     # print(sb_data.data)
-
-#     sb.save_data_file_pkl(sb_data.data, "softbank-2022_11_26_14_50_49")
-#     # sb.save_data_file_pkl(sb_data.data, "softbank_test")
+    #     sb.save_data_file_pkl(sb_data.data, "softbank-2022_11_26_14_50_49")
+    #     # sb.save_data_file_pkl(sb_data.data, "softbank_test")
 
 
-def fix_data(src_file="softbank_test"):
-    data = load_data_from_pkl_file(src_file)
-    # print(data)
-    for area, class_info_xs in data.items():
-        for class_info in class_info_xs.values():
-            if class_info["has_parking"] in ["None", "－"]:
-                class_info["has_parking"] = False
-            else:
-                class_info["has_parking"] = True
+    @classmethod
+    def fix_data(cls, src_file="softbank_test"):
+        data = cls.load_data_from_pkl_file(src_file)
+        # print(data)
+        for area, class_info_xs in data.items():
+            for class_info in class_info_xs.values():
+                if class_info["has_parking"] in ["None", "－"]:
+                    class_info["has_parking"] = False
+                else:
+                    class_info["has_parking"] = True
 
-            if class_info["is_barrier_free"] == "－":
-                class_info["is_barrier_free"] = False
-            else:
-                class_info["is_barrier_free"] = True
+                if class_info["is_barrier_free"] == "－":
+                    class_info["is_barrier_free"] = False
+                else:
+                    class_info["is_barrier_free"] = True
 
-    # print(data)
-    sb.save_data_file_pkl(data, file_path=src_file + "_fixed")
-    sb.quit_driver()
+        # print(data)
+        cls.save_data_to_pkl_file(data, file_path=src_file + "_fixed")
